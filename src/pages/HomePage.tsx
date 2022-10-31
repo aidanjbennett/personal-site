c3ecadf448a2685c97fab9089e856bcdb81e25be
Aidan James Bennett <aidanbennett1103@gmail.com>
2022-10-31 11:34:47 +0000

    Add title to home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 export default function HomePage(): JSX.Element {
   return (
-    <div>
-      <h1>HomePage</h1>
-    </div>
+    <>
+      <Helmet>
+        <title>Home | Aidan Bennett</title>
+      </Helmet>
+      <div>
+        <h1>Hi, I'm Aidan Bennett</h1>
+      </div>
+    </>
   );
 }
